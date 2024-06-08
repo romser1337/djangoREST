f149b37f772ca8da04fd278fa61a9c9db805d35e
romser1337 <romser1337@gmail.com>
2024-06-08 10:59:56 +0300

    Add getCells

--- rest/geo_gateway/urls.py
+++ rest/geo_gateway/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('geodata',views.getData),
+    path('geodata', views.getData),
+    path('get-cells', views.getCells),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- rest/geo_gateway/views.py
+++ rest/geo_gateway/views.py
@@ -11,13 +11,28 @@ import json
 
 @api_view(['GET'])
 def getData(request):
-    QueryDict = request.query_params
     conn = ptp.connect()
-
-    query2 = """select geojson from geo_test.coverage_analytics ca where 1=1 uid = '1'"""
+    query = """select geojson from geo_test.coverage_analytics ca where uid = '1'"""
     cursor = conn.cursor()
-    cursor.execute(query2)
+    cursor.execute(query)
     tuples_list = cursor.fetchall()
     response = json.dumps(tuples_list[0][0])
     return Response(json.loads(response))
 
+
+@api_view(['GET'])
+def getCells(request):
+    conn = ptp.connect()
+    query = """select geojson from geo_test.cells c where uid = '3'"""
+    cursor = conn.cursor()
+    cursor.execute(query)
+    tuples_list = cursor.fetchall()
+    response = json.dumps(tuples_list[0][0])
+    return Response(json.loads(response))
+
+
+@api_view(['GET'])
+def dismantleSIte(request):
+    # some code
+    # geojson =  function(siteid)
+    return Response(json.loads())
