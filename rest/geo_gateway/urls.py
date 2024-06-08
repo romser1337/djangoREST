a504af08696eb5526aba8f3c9fec4a18a9bd0607
romser1337 <romser1337@gmail.com>
2024-06-08 12:47:03 +0300

    Add url for dismantle site Fix function cases

--- rest/geo_gateway/urls.py
+++ rest/geo_gateway/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('geodata', views.getData),
-    path('get-cells', views.getCells),
+    path('geodata', views.get_coverage),
+    path('get-cells', views.get_cells),
+    path('dismantle-site', views.dismantle_site),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- rest/geo_gateway/views.py
+++ rest/geo_gateway/views.py
@@ -2,15 +2,12 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from . import posgre_to_pd as ptp
-import pandas as pd
-import geopandas
 import json
-
 
 # Create your views here.
 
 @api_view(['GET'])
-def getData(request):
+def get_coverage(request):
     conn = ptp.connect()
     query = """select geojson from geo_test.coverage_analytics ca where uid = '1'"""
     cursor = conn.cursor()
@@ -21,7 +18,7 @@ def getData(request):
 
 
 @api_view(['GET'])
-def getCells(request):
+def get_cells(request):
     conn = ptp.connect()
     query = """select geojson from geo_test.cells c where uid = '3'"""
     cursor = conn.cursor()
@@ -32,7 +29,7 @@ def getCells(request):
 
 
 @api_view(['GET'])
-def dismantleSIte(request):
+def dismantle_site(request):
     # some code
-    # geojson =  function(siteid)
+    # geojson = function(siteid)
     return Response(json.loads())
