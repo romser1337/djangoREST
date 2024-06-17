aec2e5742137ce2105ecaa27464a990368b7c2b2
romser1337 <romser1337@gmail.com>
2024-06-17 13:37:41 +0300

    function to add raster

--- rest/geo_gateway/views.py
+++ rest/geo_gateway/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from geo.Geoserver import Geoserver
+import os
 from . import posgre_to_pd as ptp
 import json
 
@@ -32,6 +33,11 @@ def get_cells(request):
 def dismantle_site(request):
     cells = request.GET.getlist('cells[]', [])
     sites = request.GET.getlist('sites[]', [])
-    # some code
+    # publishing geolayer
+    geo = Geoserver('http://geoserver:8080/geoserver', username='admin', password='geoserver')
+    geo.create_coveragestore(layer_name='layer5',
+                             path='/rest/geo_gateway/static/raster_test_output_rgba.tif',
+                             workspace='demo')
+
     # geojson = function(siteid)
-    return Response(cells)
+    return Response({'workspace': 'demo', 'layer': 'layer5'})
